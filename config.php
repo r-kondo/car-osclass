493bc010814adf9715cfe521dbd68a9d4cdcdd24
Ryota Kondo <thankstohome@gmail.com>
2017-04-13 00:15:57 +0900

    refs #159 要求依頼事項 不具合を起こす要素やオーバースペックになりそうなプラグインを削除

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'marketengin_osdb');
+
+/** MySQL database username */
+define('DB_USER', 'marketengin_oscl');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'je42nlo0');
+
+/** MySQL hostname */
+define('DB_HOST', 'mysql1110.xserver.jp');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/dev_financial-cars/');
+
+define('WEB_PATH', 'http://market-engin.xsrv.jp/dev_financial-cars/');
+
+?>
